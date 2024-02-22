bd2946d8e0c21c633cad55d925124b005a8c51b3
Agnes <agypeter97@gmail.com>
2024-02-23 00:24:14 +0300

    app root routing config

--- admin/asgi.py
+++ admin/asgi.py
@@ -1,19 +1,11 @@
-"""
-ASGI config for admin project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 
 import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from decouple import config
 from django.core.asgi import get_asgi_application
+from decouple import config
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
 
@@ -21,9 +13,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+import adminApp.routing
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # We will add WebSocket protocol later. For now, it's just HTTP.
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(adminApp.routing.websocket_urlpatterns))
+        ),
     }
 )
