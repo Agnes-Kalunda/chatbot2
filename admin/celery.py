1dbd4c5e05cd6f563df218fc4734c9bdd9c87320
Agnes <agypeter97@gmail.com>
2024-02-23 23:46:03 +0300

    celery for the automated responses

--- admin/celery.py
+++ admin/celery.py
@@ -1,9 +1,9 @@
 import os
 
-from decouple import config
+
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- admin/settings.py
+++ admin/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     "chatterbot.ext.django_chatterbot",
+    'chatterbot.ext.django_chatterbot',
     'adminApp',
     'tailwind',
     'theme',
